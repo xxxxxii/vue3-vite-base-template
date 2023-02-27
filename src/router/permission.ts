632c18e1022d6d8c34e806ffc0b9b48c6426a640
xxxxxii <1973329248@qq.com>
2023-02-27 13:25:58 +0800

    配置修改

--- src/router/permission.ts
+++ src/router/permission.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: yulinZ 1973329248@qq.com
  * @Date: 2022-10-18 20:49:53
- * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2023-02-27 02:16:32
+ * @LastEditors: xxxxxii 1973329248@qq.com
+ * @LastEditTime: 2023-02-27 11:47:06
  * @FilePath: \vue3-element-admin\src\permission.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -15,7 +15,7 @@ const whiteList = [""];
 export function guard(router: Router) {
   // 进入前
   router.beforeEach(async (to, from, next) => {
-    console.log("路由守卫：", to, from, next);
+    console.log("路由守卫：", to, from);
     next();
   });
 
